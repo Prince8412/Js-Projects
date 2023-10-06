96f903a39c79e03c5323c8e1b06c8368bbbde399
prince raj <100460531+Prince8412@users.noreply.github.com>
2023-10-06 23:37:55 +0530

    Insorts Clone

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+
+// Components
+import Header from './components/Header'; // Here we used ./ because we have to import file only form the same forlder
+// but if we had to import for different folder then we weoud use ../
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( <
+        div className = "App" >
+        <
+        Header / >
+        <
+        /div>
+    );
 }
 
 export default App;
